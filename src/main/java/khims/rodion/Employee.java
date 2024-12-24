c993ecc0b2f5860507bb57262be515d11f303dbd
RichardKhims <khims.richard@gmail.com>
2024-12-24 23:19:56 +0400

    employees

---
+++ src/main/java/khims/rodion/Employee.java
@@ -0,0 +1,31 @@
+package khims.rodion;
+
+public class Employee {
+    private String fio;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String fio, String position, String email, String phone, double salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fio='" + fio + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
