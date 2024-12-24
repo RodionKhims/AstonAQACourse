87a2ecc34f9065375d0c28b842b823b8ea107749
RichardKhims <khims.richard@gmail.com>
2024-12-24 21:17:24 +0400

    fix

--- src/main/java/khims/rodion/Program.java
+++ src/main/java/khims/rodion/Program.java
@@ -82,7 +82,7 @@ public class Program {
     private static void replaceZeroOne() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (arr[i] + 1) % 2;
+            arr[i] = arr[i] == 0 ? 1 : 0;
         }
     }
 
