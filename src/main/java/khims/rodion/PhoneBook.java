1923a2579d9a0563f6fe021f1f0ef25b7fd2f67e
RichardKhims <khims.richard@gmail.com>
2025-01-22 02:27:43 +0400

    lesson 8

---
+++ src/main/java/khims/rodion/PhoneBook.java
@@ -0,0 +1,21 @@
+package khims.rodion;
+
+import java.util.*;
+
+public class PhoneBook {
+    private final Map<String, List<String>> phoneMap = new HashMap<>();
+
+    public void add(String name, String phone) {
+        Optional.ofNullable(phoneMap.get(name)).ifPresentOrElse(
+                list -> list.add(phone),
+                () -> {
+                    List<String> phones = new ArrayList<>();
+                    phones.add(phone);
+                    phoneMap.put(name, phones);
+                });
+    }
+
+    public List<String> getPhones(String name) {
+        return phoneMap.getOrDefault(name, List.of());
+    }
+}
