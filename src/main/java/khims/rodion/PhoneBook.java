7135ebbf70abd432c37f95997b04022f5cf873e7
RichardKhims <khims.richard@gmail.com>
2025-01-26 01:46:13 +0400

    fix

--- src/main/java/khims/rodion/PhoneBook.java
+++ src/main/java/khims/rodion/PhoneBook.java
@@ -6,8 +6,8 @@ public class PhoneBook {
     private final Map<String, List<String>> phoneMap = new HashMap<>();
 
     public void add(String name, String phone) {
-        phoneMap.putIfAbsent(name, new ArrayList<>());
-        phoneMap.get(name).add(phone);
+        phoneMap.computeIfAbsent(name, key -> new ArrayList<>())
+                .add(phone);
     }
 
     public List<String> getPhones(String name) {
