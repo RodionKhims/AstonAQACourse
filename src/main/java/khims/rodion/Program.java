4b51eff23cbf59d2046514200dd41f88ce778667
RichardKhims <khims.richard@gmail.com>
2025-02-11 19:57:08 +0400

    lesson_13

--- src/main/java/khims/rodion/Program.java
+++ src/main/java/khims/rodion/Program.java
@@ -2,6 +2,9 @@ package khims.rodion;
 
 public class Program {
     public static void main(String[] args) {
-
+        System.out.println(MathUtils.calcFactorial(10));
+        System.out.println(MathUtils.calcFactorial(5));
+        System.out.println(MathUtils.calcFactorial(0));
+        System.out.println(MathUtils.calcFactorial(-1));
     }
 }
